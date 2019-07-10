3192f4381e4baed8cadea9b4944e436c392da886
Chris Troutner <chris.troutner@gmail.com>
2019-07-10 19:16:52 +0000

    Forked from https://github.com/christroutner/koa-ipfs-blog

---
+++ production/common.js
@@ -0,0 +1,9 @@
+/*
+  This file is used to store unsecure, application-specific data common to all
+  environments.
+*/
+
+module.exports = {
+  port: process.env.PORT || 3210,
+  BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`
+}
