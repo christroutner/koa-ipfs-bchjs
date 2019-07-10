b90f5998d85392bb80f467d600b28c98fe4e70be
Chris Troutner <chris.troutner@gmail.com>
2019-07-10 13:45:17 -0700

    Adding correct bch addr to production

--- production/common.js
+++ production/common.js
@@ -5,5 +5,5 @@
 
 module.exports = {
   port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`
+  BCHADDR: `bitcoincash:qpyrsf8uf3qr7m23h4hdhjlj2cmxet6q5g0mjhdrqu`
 }
